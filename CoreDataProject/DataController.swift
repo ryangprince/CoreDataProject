e183616adca6ed7c2fb575b57c85f5362bea3a34
Ryan Prince <ryanprince@macbook-pro.myfiosgateway.com>
2022-10-19 22:01:12 -0400

    first commit

---
+++ CoreDataProject/DataController.swift
@@ -0,0 +1,24 @@
+//
+//  DataController.swift
+//  CoreDataProject
+//
+//  Created by Ryan Prince on 10/15/22.
+//
+
+import CoreData
+import Foundation
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "CoreDataProject")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core data failed to load: \(error.localizedDescription)")
+                return
+            }
+            
+            self.container.viewContext.mergePolicy = NSMergePolicy.mergeByPropertyObjectTrump
+        }
+    }
+}
---
+++ CoreDataProject/Singer+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Singer+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Ryan Prince on 10/19/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Singer)
+public class Singer: NSManagedObject {
+
+}
